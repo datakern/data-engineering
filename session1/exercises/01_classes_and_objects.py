17d28977443a9b4d7afe354193db51657e5d10f7
Teja <p.saitejasri@gmail.com>
2026-09-12 08:33:06 +0200

    First exercise by Teja

--- session1/exercises/01_classes_and_objects.py
+++ session1/exercises/01_classes_and_objects.py
@@ -12,10 +12,9 @@ Instructions:
 
 # 1. Define the class here
 class DatabaseConnection:
-    # YOUR CODE HERE
     pass
 
 # 2. Create the object here
 if __name__ == "__main__":
-    # YOUR CODE HERE
-    pass
+    my_db = DatabaseConnection()
+    print(my_db)
